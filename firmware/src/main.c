6700f8cef817f55c0f642a60619fb8e6845c5d70
Gergely Koloszar <gergely.koloszar@gmail.com>
2023-02-02 20:12:53 +0100

    Add basic firmware project

---
+++ firmware/src/main.c
@@ -0,0 +1,19 @@
+// standard includes
+#include <stdint.h>
+
+// mcu specific includes
+
+// FreeRTOS includes
+
+// FreeRTOS port
+
+// project includes
+
+int main (void){
+
+  // begin and init, possible OS start
+
+  while(1){
+    // main infinite loop
+  }
+}
